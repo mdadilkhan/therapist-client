1cd52350ccca7e8fa3ea0410fe95766c6bbb2b2d
mdadilkhan <mdadilakhtar8@gmail.com>
2025-02-07 14:40:15 +0530

    change vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import federation from "@originjs/vite-plugin-federation";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-     federation({
-      name: "corporate_counsellor",
-      filename: "corporateCounsellor.js",
-      exposes: {
-        "./corporate_counsellor": "./src/app.jsx",
-        "./counsellorStore": "./src/store/store.jsx",
-      },
-      shared: ['react','react-router-dom','react-dom', 'react-redux', '@reduxjs/toolkit','dayjs'],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
-    minify: false,
-    cssCodeSplit: false,
-  },
-});
+  plugins: [react()],
+})
